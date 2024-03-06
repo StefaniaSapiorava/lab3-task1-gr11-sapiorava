60239d9a8c4dae9fca826d7af721d226c2ea7716
StefaniaSapiorava <sapioravastefania@gmail.com>
2024-03-06 13:37:44 +0000

    Добавлена функция проверки чисел

--- src/main.c
+++ src/main.c
@@ -3,6 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int isHappyNumber(int number) {
+    int digit1 = number / 100000;
+    int digit2 = (number / 10000) % 10;
+    int digit3 = (number / 1000) % 10;
+    int digit4 = (number / 100) % 10;
+    int digit5 = (number / 10) % 10;
+    int digit6 = number % 10;
+
+    return (digit1 + digit2 + digit3 == digit4 + digit5 + digit6);
+}
+
 int main(int argc, char *argv[]) {
     // Проверка, что передано два аргумента командной строки
     if (argc != 3) {
